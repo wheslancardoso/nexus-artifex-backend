bd2d4b76bfa7098fa273bdbd511eda7126409948
wheslancardoso <wheslancardoso1@gmail.com>
2025-12-13 16:21:08 -0300

    first commit

---
+++ src/main/java/com/nexusartifex/nexus_artifex_backend/NexusArtifexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexusartifex.nexus_artifex_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusArtifexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusArtifexBackendApplication.class, args);
+	}
+
+}
